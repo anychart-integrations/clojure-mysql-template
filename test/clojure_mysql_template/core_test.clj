1e7745e58bba1ba6ae80ba9804abeb48537a3751
ilevd <ilevd@mail.ru>
2018-10-17 15:12:08 +0700

    Delete unused files

--- test/clojure_mysql_template/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-mysql-template.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-mysql-template.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
